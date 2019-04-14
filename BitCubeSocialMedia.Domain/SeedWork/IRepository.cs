33394ec9c391aa1aa954b0dc810e076d31d620cd
= <=>
2019-04-14 19:25:57 +0200

    Starting Repository implementation

---
+++ BitCubeSocialMedia.Domain/SeedWork/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitCubeSocialMedia.Domain.SeedWork
+{
+    public interface IRepository<TEntity, TId> where TEntity : class, IEntity
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetById(TId id);
+        Task Insert(TEntity entity);
+        void Update(TEntity entity);
+        Task Delete(TEntity entity);
+    }
+}
