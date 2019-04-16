63398d63000f2355692238d90fa5c6cac609200f
= <=>
2019-04-16 12:23:56 +0200

    Implemented Authentication

--- BitCubeSocialMedia.Infrastructure/Implementations/Logics/Authentication.cs
+++ BitCubeSocialMedia.Infrastructure/Implementations/Logics/Authentication.cs
@@ -1,25 +1,31 @@
 ﻿using BitCubeSocialMedia.Application.ILogic;
 using BitCubeSocialMedia.Application.Models.Auth;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using AutoMapper;
 
 namespace BitCubeSocialMedia.Infrastructure.Implementations.Logics
 {
-    public class Authentication : IAuthentication
+    public class Authentication : ControllerBase, IAuthentication
     {
         #region Fields
         private readonly IUserLogic _userLogic;
+        private readonly IMapper _mapper;
         #endregion Fields
 
         #region Constructors
-        public Authentication(IUserLogic userLogic)
+        public Authentication(IUserLogic userLogic, IMapper mapper)
         {
             _userLogic = userLogic;
+            _mapper = mapper;
         }
         #endregion Constructors
 
@@ -34,17 +40,31 @@ namespace BitCubeSocialMedia.Infrastructure.Implementations.Logics
                     new Claim("name", signInModel.Email)
                 };
                 var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme, "name", null);
+                var principle = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync(principle);
+            }
+            else
+            {
+                //TODO: Throw custom exception
             }
         }
 
-        public Task SignoutUser()
+        public async Task SignoutUser()
         {
-            throw new NotImplementedException();
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
         }
 
-        public Task SignUpUser(SignUpModel signUpModel)
+        public async Task SignUpUser(SignUpModel signUpModel)
         {
-            throw new NotImplementedException();
+            if(await _userLogic.AddUser(signUpModel))
+            {
+                var signInModel = _mapper.Map<SignInModel>(signUpModel);
+                await SignInUser(signInModel);
+            }
+            else
+            {
+                //TODO: Throw custom exception
+            }
         }
         #endregion Methods
     }
