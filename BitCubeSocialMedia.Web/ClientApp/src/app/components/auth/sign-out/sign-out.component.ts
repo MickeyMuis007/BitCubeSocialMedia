bdadb7c5d697e847c9c5ace78ed40fac4394ab1e
= <=>
2019-04-18 19:59:12 +0200

    Creating temp sign-out page

--- BitCubeSocialMedia.Web/ClientApp/src/app/components/auth/sign-out/sign-out.component.ts
+++ BitCubeSocialMedia.Web/ClientApp/src/app/components/auth/sign-out/sign-out.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: './sign-out.component.html'
+  templateUrl: './sign-out.component.html',
+  styleUrls:  ['./sign-out.component.css']
 })
 export class SignOutComponent {
 
