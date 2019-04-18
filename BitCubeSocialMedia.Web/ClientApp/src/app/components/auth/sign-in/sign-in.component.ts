318bb848f9f44da02d0081282db2e9d1ef3bbdd3
= <=>
2019-04-18 09:58:28 +0200

    Creating sign-in UI

--- BitCubeSocialMedia.Web/ClientApp/src/app/components/auth/sign-in/sign-in.component.ts
+++ BitCubeSocialMedia.Web/ClientApp/src/app/components/auth/sign-in/sign-in.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './sign-in.component.html'
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
+  signInFormGroup: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  ngOnInit() { 
+
+    this.buildFormGroup();
+  }
+
+  save() {
+    if (this.signInFormGroup.valid) {
+      console.log("Valid");
+    }
+    else {
+      console.log("Invalid")
+    }
+  }
+
+  private buildFormGroup() {
+    this.signInFormGroup = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.required ]
+    });
+
+  }
 }
