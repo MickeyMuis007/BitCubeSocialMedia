80a9b2e13c9294c87217273b86cf30022bade327
= <=>
2019-04-22 13:21:16 +0200

    Add password miss match validation UI

--- BitCubeSocialMedia.Web/ClientApp/src/app/components/auth/sign-up/sign-up.component.ts
+++ BitCubeSocialMedia.Web/ClientApp/src/app/components/auth/sign-up/sign-up.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ConfirmPasswordValidator } from 'src/app/shared/validators/confirm-password.validator';
 
 @Component({
   templateUrl: './sign-up.component.html'
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  signUpForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.buildFormGroup();
+  }
+
+  save() {
+
+  }
+
+  private buildFormGroup() {
+    this.signUpForm = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+      confirmPassword: ['']
+    }, {
+        validator: ConfirmPasswordValidator.MatchPassword
+    });
+  }
 }
